0c0add8852de554071c4b8e2b008b61dbc32659d
saneki <s@neki.me>
2021-11-16 21:32:02 -0600

    nonvalue.hxx: Restyle

--- include/nonsense/nonvalue.hxx
+++ include/nonsense/nonvalue.hxx
@@ -5,55 +5,49 @@
 #include <type_traits>
 
 namespace nonsense {
-	template <typename T, T Exempt>
-	class NonValue {
-	public:
-		constexpr NonValue() = default;
 
-		constexpr NonValue(T value)
-			: m_value(value)
-		{
-		}
+template <typename T, T Exempt>
+class non_value {
+public:
+	constexpr non_value() = default;
 
-		/**
-		 * Return the underlying value, regardless of whether or not it contains the exempt value.
-		 **/
-		constexpr T operator*() const noexcept {
-			return m_value;
-		}
+	constexpr non_value(T value)
+		: _value(value) {}
 
-		/**
-		 * Return the underlying value or throw an exception if it contains the exempt value.
-		 **/
-		constexpr T get() const {
-			if (hasValue())
-				return m_value;
-			else
-				throw std::runtime_error("Does not contain non-exempt value.");
-		}
+	// Return the underlying value, regardless of whether or not it contains the exempt value.
+	constexpr T operator*() const noexcept {
+		return _value;
+	}
 
-		/**
-		 * Whether or not the contained value is distinct from the exempt value.
-		 **/
-		constexpr bool hasValue() const noexcept {
-			return m_value != Exempt;
-		}
-	private:
-		T m_value = Exempt;
-	};
+	// Return the underlying value or throw an exception if it contains the exempt value.
+	constexpr T get() const {
+		if (has_value())
+			return _value;
+		else
+			throw std::runtime_error("Does not contain non-exempt value.");
+	}
 
-	template <typename T>
-	using NonZero = NonValue<T, 0>;
+	// Whether or not the contained value is distinct from the exempt value.
+	constexpr bool has_value() const noexcept {
+		return _value != Exempt;
+	}
+private:
+	T _value = Exempt;
+};
 
-	template <typename T>
-	using NonMax = NonValue<T, std::numeric_limits<T>::max()>;
+template <typename T>
+using non_zero = non_value<T, 0>;
 
-	template <typename T>
-	using NonMin = NonValue<T, std::numeric_limits<T>::min()>;
+template <typename T>
+using non_max = non_value<T, std::numeric_limits<T>::max()>;
 
-	template <typename T>
-	using NonLowest = NonValue<T, std::numeric_limits<T>::lowest()>;
+template <typename T>
+using non_min = non_value<T, std::numeric_limits<T>::min()>;
 
-	template <typename T, std::enable_if_t<std::is_pointer_v<T>, bool> = true>
-	using NonNull = NonValue<T, nullptr>;
+template <typename T>
+using non_lowest = non_value<T, std::numeric_limits<T>::lowest()>;
+
+template <typename T, std::enable_if_t<std::is_pointer_v<T>, bool> = true>
+using non_null = non_value<T, nullptr>;
+
 } // namespace nonsense
